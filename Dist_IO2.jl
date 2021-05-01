e39bf4d06d864dccfe3185f4a860c29092ebe082
alexia-torres <40325960+alexia-torres@users.noreply.github.com>
2021-05-01 11:40:22 -0500

    final version hola ..wq
    final commir

--- Dist_IO2.jl
+++ Dist_IO2.jl
@@ -545,8 +545,44 @@ md" Como podemos ver, mientras más veces repetimos el experimento, hay mayor pa
 # ╔═╡ 8b338b49-1dbf-40f3-889a-157804c87a50
 begin
 	scatter(data_geo, xticks=false,legend=false)
-	hline!([mean(data_geo)])
+	hline!([mean(data_geo)],lw=3, ls=:dash, c=:red,leg=false)
+end
+
+# ╔═╡ 8048479d-5a08-401b-a1a0-b7c501b82927
+Markdown.parse("`Promedio días de recuperación:  "*string(mean(data_geo))*"`")
+
+# ╔═╡ 97dd5cb6-d360-4f49-b20a-9cb260e2e5b0
+begin
+	m=mean(data_geo)
 	
+	squared_data = (data_geo .- m).^2
+	
+	variance = mean(squared_data)
+	
+	Markdown.parse("`Varianza días de recuperación:  "*string(variance)*"`")
+end
+
+
+
+# ╔═╡ c2691808-685d-44a3-98ba-3ec930665851
+begin
+	hline!([mean(data_geo)], lw=3, ls=:dash, c=:purple,leg=false)
+	hline!([mean(data_geo)+variance], lw=3, ls=:dash, c=:green,leg=false)
+	hline!([mean(data_geo)-variance], lw=3, ls=:dash, c=:green,leg=false)
+end
+
+# ╔═╡ a3f31d9a-eb88-49e4-926f-6965c3b0d108
+begin
+	σ=sqrt(variance)
+	Markdown.parse("`Desviación estandar días de recuperación:  "*string(σ)*"`")
+end
+
+# ╔═╡ 33ec9fb2-c0a6-43c3-93de-e5376b2760e7
+begin
+	scatter(data_geo, alpha=0.5)
+	hline!([mean(data_geo)], lw=3, ls=:dash, c=:purple,leg=false)
+	hline!([mean(data_geo)+σ], lw=3, ls=:dash, c=:red,leg=false)
+	hline!([mean(data_geo)-σ], lw=3, ls=:dash, c=:red,leg=false)
 end
 
 # ╔═╡ 514df2eb-a1f9-4665-927c-7d74aa2f1505
@@ -590,15 +626,6 @@ begin
 	plot_cumulative2!(pdf_geo, label="")
 end
 
-# ╔═╡ b63b1e49-f7d2-4699-8198-29b09d92ddae
-md"
----
-### Más casos
-
-(Incluir ejemplos Indutriales, Algo para usar el Canvas)
-"
-
-
 # ╔═╡ 0b4e7950-5e5b-4056-987e-c6d93546bd48
 md"
 ---
@@ -616,7 +643,7 @@ kp(k)= \sum_{k=1}^\infty k(1-p)^{k-1}p=\frac{1}{p}\\
 \end{align*}$$"
 
 # ╔═╡ e4399d37-e193-4d64-9280-6356cb84fb71
-md"# Distribución Poisson
+md"## Distribución Poisson
 
 Caso Introductorio:
 
@@ -681,22 +708,152 @@ e^{-\lambda}e^\lambda=\lambda$
 $Var(X)=\lambda$
 "
 
-# ╔═╡ 4457a199-8b9e-4730-8f35-4e2a25ca7436
-md"# To Do's
+# ╔═╡ 9c00f83b-e97a-42ec-bc7f-5c95138d100f
+md"""
+# Distribuciones continuas
+"""
 
-- Generalizar plotcumulative!
-- Table of Content (Indice)
-- Embellecer los plots (colores, labels, tittles)
+# ╔═╡ 4c5b357e-d4c5-4f72-8b35-bbd1824da232
+md"""
+p = $(@bind p Slider(0.1:0.05:1, default=.2, show_value=true))
+"""
 
+# ╔═╡ 6c1c43e7-96d9-4c70-bc95-dfe683d4af7e
+md"""
+N = $(@bind N Slider(10:10:100, default=20, show_value=true))
+"""
 
-- Agregar caso Poisson
-- Agregar mas casos de aplicacion
-- Modelos Continuos"
+# ╔═╡ be5a0d38-f24b-40ac-92a0-faf9e608024e
+md"""
+δ = $(@bind δ Slider(0.1:0.05:1, default=1, show_value=true))
+"""
+
+# ╔═╡ 296f430f-7129-4d8f-84b2-6c398efb4ebe
+begin
+	continua_pdf(p, N) = [p * (1 - p)^(n-1) for n in 1:N]
+	plot()
+	plot_cumulative!(continua_pdf(p, N), 1, label="delta = 1")
+	plot_cumulative!(continua_pdf(p, N), δ, label="delta = $(δ)")
+end
+
+# ╔═╡ de4c95d8-894c-47b7-99ad-0959ffa674ab
+md"""
+## Exponencial
+"""
+
+# ╔═╡ d237deda-a25d-45d3-817a-7019dd0dd15c
+md"""
+λ = $(@bind λ Slider(0:1:50, default=1, show_value=true))
+"""
+
+# ╔═╡ 793a2fb0-9f23-41db-b4af-bfe363218b86
+md""" t = $(@bind t Slider(1:0.5:50, default=5, show_value=true)) """
+
+# ╔═╡ 35b8cac6-3ffc-4676-bee3-7b9c428362f9
+begin
+	densidad_exponencial(t) = λ * exp(-λ * t);
+	probabilidad_exponencial(t) = 1 - exp(-λ * t);
+end;
+
+# ╔═╡ 88870c07-b603-40de-b398-64cbed17fa38
+md"""
+Selecciona una función: $(@bind exp_func Select([
+	"Densidad", "Probabilidad"
+]))
+"""
+
+# ╔═╡ fed32500-1c8f-4875-8831-87b0ea9763de
+f(t) = exp_func == "Densidad" ? densidad_exponencial(t) : probabilidad_exponencial(t);
+
+# ╔═╡ e9939368-a08f-4994-a823-c1b6b72b9814
+range = 2:0.1:t;
+
+# ╔═╡ f48ab57e-5ca7-4be8-af30-a6626dd9ed40
+begin
+	plot()
+	plot(0:0.1:t, f)
+	plot!(range, f, fill=true, alpha=0.4, legend=false)
+end
+
+# ╔═╡ f73f3507-03ac-4957-9cba-082b61d462ef
+md"## Distribucion uniforme"
+
+# ╔═╡ 93de82c3-e3d6-47ee-bcdb-5926e1447aca
+md"Caso Introdutorio: "
+
+# ╔═╡ d8633435-5c37-4d78-89da-001efbfa6829
+uniform(a, b) = a + rand() * (b - a)
+
+# ╔═╡ ef25478e-e577-4e3c-8de8-df0f4de115e3
+@bind datos_unif NumberField(0:200, default=100)
+
+# ╔═╡ b3d48ffe-5d88-491e-bf8d-695aa17f356e
+md"Mínimo (a):"
+
+# ╔═╡ 1e6dff27-ccba-48c8-b015-411b334e63bb
+@bind unif_min NumberField(0:200, default=0)
+
+# ╔═╡ f46bfb4c-93b1-4fc6-9e8e-4579996ed3fb
+md"Máximo (b):"
+
+# ╔═╡ 4c25ea05-a4b0-4cb3-98f0-b19ffb75f2a8
+@bind unif_max NumberField(0:200, default=1)
+
+# ╔═╡ 8c993fe9-2719-4825-adf7-fc07c43b349d
+begin
+	data_unif = [uniform(unif_min, unif_max) for i in 1:datos_unif];
+	histogram(data_unif, nbins=20, legend=false)  # function in Plots.jl
+	scatter!(data_unif, zero.(data_unif), ms=2, alpha=0.3)
+end
+
+# ╔═╡ c0c22c1f-7508-40bd-bda7-afaeda8d58b2
+md"
+---
+### Exploremos la teoría
+"
+
+# ╔═╡ 8e7dba6d-8f35-47d8-afda-2c36d1313a4a
+md"
+###### Función de densidad de probabilidad:
+$$
+\begin{align*}
+f(x) &= \begin{cases}
+\frac{1}{b - a} & \text{ si } a \leq x \leq b \\\\ 
+              0 & \text{ fuera }\\\\
+        \end{cases}
+\end{align*}$$"
+
+# ╔═╡ 2829a895-7de5-47dd-a97a-5c9b2da1a59e
+md"Coloca el mínimo y máximo"
+
+# ╔═╡ 93c6323a-7dac-4174-aea5-139cc38283f4
+@bind a NumberField(0:200, default=0)
+
+# ╔═╡ 61a15261-b13f-46a5-9a01-3c18d0245e23
+@bind b NumberField(0:200, default=1)
+
+# ╔═╡ e9b56895-7ea0-4ac6-90df-4b77bfa30e02
+begin
+	plot(vcat([a-0.2,a],[ i+0.1 for i in a-0.1:0.1:b-0.1],[b,b+0.2]), vcat([0,0],[pdf(Uniform(a,b),i) for i in a:0.1:b],[0,0]), legend=false, ylims=(0,1), yticks= [1/b-a], linewidth=3)
+end
+
+# ╔═╡ 877346f2-d025-4330-adaa-7585af22a653
+md"
+###### Función de distribución acumulada:
+$$
+\begin{align*}
+F(x) \equiv & P(X\leq x)= \int_{a}^{x}\frac{1}{b-a}dt\\\\
+          = & \begin{cases}
+                0 & \text{ si } x < a\\\\
+                \frac{x-a}{b - a} & \text{ si } a \leq x \leq b\\\\
+                1 & \text{ si } x > b
+              \end{cases}
+\end{align*}$$"
 
 # ╔═╡ Cell order:
 # ╟─aacc06b4-1afb-4e83-8392-45b2e24eb51a
 # ╠═1e264b46-fd83-4ee0-952a-544d38a7ac76
-# ╠═d0ae819a-d96a-4715-86f0-e6e480d0714b
+# ╟─d0ae819a-d96a-4715-86f0-e6e480d0714b
 # ╠═fb931c1d-3de4-489c-96d7-a69bb14fd1b2
 # ╟─efd9c782-f1af-4b37-823f-e1a8e606f434
 # ╟─91357e08-3a1c-41b3-9c03-1d839c3222ec
@@ -765,18 +922,22 @@ md"# To Do's
 # ╟─145e4c65-9e3b-4c8b-9c40-f41892fc9d7c
 # ╟─ee30e1f0-9f93-4af6-b51e-4df0de63afad
 # ╟─e7340cda-92b7-4496-a47b-a733cc2a59b9
-# ╠═bb984796-fb9f-4f93-81c8-629d1107c52e
+# ╟─bb984796-fb9f-4f93-81c8-629d1107c52e
 # ╟─c009f68e-9d39-4d41-8f47-0620ddae717c
-# ╠═8b338b49-1dbf-40f3-889a-157804c87a50
+# ╟─8b338b49-1dbf-40f3-889a-157804c87a50
+# ╟─8048479d-5a08-401b-a1a0-b7c501b82927
+# ╟─97dd5cb6-d360-4f49-b20a-9cb260e2e5b0
+# ╟─c2691808-685d-44a3-98ba-3ec930665851
+# ╟─a3f31d9a-eb88-49e4-926f-6965c3b0d108
+# ╟─33ec9fb2-c0a6-43c3-93de-e5376b2760e7
 # ╟─514df2eb-a1f9-4665-927c-7d74aa2f1505
 # ╟─d9ea4012-45ee-4baf-bf4f-a2b0ec71b69c
 # ╟─7c63a28c-9599-44eb-a144-45ec2eff4dfb
 # ╟─f72fd27b-3f9a-4f3f-a412-71b37797d4dd
-# ╠═81ba5f86-6762-4b8f-ba64-a4779c7c65d1
+# ╟─81ba5f86-6762-4b8f-ba64-a4779c7c65d1
 # ╟─23eead02-a4bd-43b3-9dfb-0f83a9acc3ef
 # ╟─21a292db-3871-4133-8396-67e5ebc1c509
-# ╟─b63b1e49-f7d2-4699-8198-29b09d92ddae
-# ╠═0b4e7950-5e5b-4056-987e-c6d93546bd48
+# ╟─0b4e7950-5e5b-4056-987e-c6d93546bd48
 # ╟─e4399d37-e193-4d64-9280-6356cb84fb71
 # ╟─f61f9db3-b6a5-4dbf-a195-3d9cb3cbf9b3
 # ╟─8903b303-2926-4064-9a58-d9d6329883fd
@@ -786,4 +947,32 @@ md"# To Do's
 # ╟─153ad89f-de01-4963-91cb-fe2c2b842eb9
 # ╟─9092b7a7-d5bd-45bc-99a1-19f545aacb1d
 # ╟─7b4b1265-83af-42c6-86dc-70126460bfde
-# ╟─4457a199-8b9e-4730-8f35-4e2a25ca7436
+# ╟─9c00f83b-e97a-42ec-bc7f-5c95138d100f
+# ╟─4c5b357e-d4c5-4f72-8b35-bbd1824da232
+# ╟─6c1c43e7-96d9-4c70-bc95-dfe683d4af7e
+# ╟─be5a0d38-f24b-40ac-92a0-faf9e608024e
+# ╟─296f430f-7129-4d8f-84b2-6c398efb4ebe
+# ╟─de4c95d8-894c-47b7-99ad-0959ffa674ab
+# ╟─d237deda-a25d-45d3-817a-7019dd0dd15c
+# ╟─793a2fb0-9f23-41db-b4af-bfe363218b86
+# ╠═35b8cac6-3ffc-4676-bee3-7b9c428362f9
+# ╟─88870c07-b603-40de-b398-64cbed17fa38
+# ╟─fed32500-1c8f-4875-8831-87b0ea9763de
+# ╠═e9939368-a08f-4994-a823-c1b6b72b9814
+# ╟─f48ab57e-5ca7-4be8-af30-a6626dd9ed40
+# ╟─f73f3507-03ac-4957-9cba-082b61d462ef
+# ╟─93de82c3-e3d6-47ee-bcdb-5926e1447aca
+# ╠═d8633435-5c37-4d78-89da-001efbfa6829
+# ╟─ef25478e-e577-4e3c-8de8-df0f4de115e3
+# ╟─b3d48ffe-5d88-491e-bf8d-695aa17f356e
+# ╟─1e6dff27-ccba-48c8-b015-411b334e63bb
+# ╟─f46bfb4c-93b1-4fc6-9e8e-4579996ed3fb
+# ╟─4c25ea05-a4b0-4cb3-98f0-b19ffb75f2a8
+# ╟─8c993fe9-2719-4825-adf7-fc07c43b349d
+# ╟─c0c22c1f-7508-40bd-bda7-afaeda8d58b2
+# ╟─8e7dba6d-8f35-47d8-afda-2c36d1313a4a
+# ╟─2829a895-7de5-47dd-a97a-5c9b2da1a59e
+# ╟─93c6323a-7dac-4174-aea5-139cc38283f4
+# ╟─61a15261-b13f-46a5-9a01-3c18d0245e23
+# ╟─e9b56895-7ea0-4ac6-90df-4b77bfa30e02
+# ╟─877346f2-d025-4330-adaa-7585af22a653
